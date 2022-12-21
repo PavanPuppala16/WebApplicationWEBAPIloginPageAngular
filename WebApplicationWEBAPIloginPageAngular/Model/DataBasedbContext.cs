0b4738f295468167384a9e927d579dee18dc7af1
PavanPuppala16 <pavankumarp@fixitytech.com>
2022-12-21 22:37:16 +0530

    Updated Files

---
+++ WebApplicationWEBAPIloginPageAngular/Model/DataBasedbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using WebApplicationWEBAPIloginPageAngular.Model;
+
+namespace WebApplicationWEBAPIloginPageAngular.Model
+{
+    public class DataBasedbContext:DbContext
+    {
+
+        public DataBasedbContext()
+        {
+
+        }
+        public DataBasedbContext(DbContextOptions<DataBasedbContext> options) : base(options)
+        {
+
+        }
+
+      public virtual DbSet<TblRegistration> TblRegistration { get; set; }
+
+
+    }
+}
---
+++ WebApplicationWEBAPIloginPageAngular/Model/TblRegistration.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplicationWEBAPIloginPageAngular.Model
+{
+    public class TblRegistration
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; } 
+        public string Name { get; set; }
+
+        public string EmailId { get; set; } 
+        public string Password { get; set; }   
+        public string Gender { get; set; }  
+        public DateTime Dob { get; set; } 
+
+
+    }
+}
